28a48a6b5afd2fadf8be71862fb341ee9535eda8
sjxiong <sjxiong@gmail.com>
2016-10-07 20:38:27 +0200

    wip

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,107 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+
+/*var awesomeThoughts = "I am Sophia and I am AWESOME!";
+var funthoughts = awesomeThoughts.replace('AWESOME','fun');
+$("#main").append(funthoughts);
+console.log(awesomeThoughts);
+
+var formattedname = HTMLheaderName.replace('%data%','sophia');
+var formattedrole = HTMLheaderRole.replace('%data%','Web Developer');
+console.log(formattedname);
+$("#header").prepend(formattedname,formattedrole);
+*/
+
+
+
+var work = {
+"jobs":[
+    {
+        "employer":"employer1",
+        "title":"title1",
+        "location":"location1",
+        "dates":"date1",
+        "description":"description1"
+    },
+    {
+        "employer":"employer2",
+        "title":"title2",
+        "location":"location2",
+        "dates":"date2",
+        "description":"description2"
+    }
+        ]
+};
+
+var projects = {
+    "projects":[
+        {
+        "title":"title1",
+        "date":"date1",
+        "description":"description1",
+        "images":["URL1","URL2","URL3"]
+        },
+        {
+        "title":"title2",
+        "date":"date2",
+        "description":"description2",
+        "images":["URL1","URL2","URL3"]
+        }
+
+                ]
+};
+
+var bio = {
+    "name":"Sophia",
+    "role":"web Developer",
+    "welcomeMessage":"I'm Sophia",
+    "biopic":"images/fry.jpg",
+    "contacts":{
+        "mobile":18675510082,
+        "email":"sjxiong@gmail.com",
+        "github":"sjxiong",
+        "twitter":"@sjxiong",
+        "location":"soest"
+    },
+    "skills":["html","CSS","javascript","dutch"]
+};
+
+
+var education = {
+    "school":[
+        {
+        "name":"school1",
+        "location":"location1",
+        "DegreeDate" : 1999,
+        "URL":"www.school1.com",
+        "majors":["major1","major2","major3"]
+        },
+        {
+        "name":"school2",
+        "location":"location2",
+        "DegreeDate" : 2002,
+        "URL":"www.school2.com",
+        "majors":["major1","major2","major3"]
+        }
+    ],
+
+    "education":[
+        {
+        "onlineCoursesTitle":"course1",
+        "school":"school1",
+        "date":"date1",
+        "URL":"www.course1.com"
+        },
+        {
+        "onlineCoursesTitle":"course2",
+        "school":"school2",
+        "date":"date2",
+        "URL":"www.course2.com"}
+    ]
+};
+
+
+
+
